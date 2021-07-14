85518449d9a7fc4ebb00b72c35f1f7e525852088
Kiefer Levine <kiefer_8@hotmail.com>
2021-07-14 14:49:43 -0400

    Added comment functionality

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -95,12 +95,16 @@ router.post('/', (req, res) => {
 
 // upvote a post
 router.put('/upvote', (req, res) => {
-    Post.upvote(req.body, { Vote })
-        .then(updatedPostData => res.json(updatedPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+    // make sure the session exists first
+    if (req.session) {
+        // pass the session id along with all destructured properties on req.body
+        Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+            .then(updatedPostData => res.json(updatedPostData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
 });
 
 // update a post
